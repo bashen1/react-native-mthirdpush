bc51562aff1a4afe885e431efe4f1344fcbefab4
bashen1 <maochunjie@gmail.com>
2020-07-18 22:02:13 +0800

    添加移除监听事件，方式安卓的热启动会监听多个

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 import {NativeModules, DeviceEventEmitter} from 'react-native';
-
 const {RNReactNativeMthirdpush} = NativeModules;
+const listeners = {};
+class Mthirdpush {
+    static notifyThirdJSDidLoad=(cb)=>{
+        return RNReactNativeMthirdpush.notifyThirdJSDidLoad(resultCode => {
+            cb(resultCode)
+        });
+    }
 
-export function notifyThirdJSDidLoad(cb) {
-    return RNReactNativeMthirdpush.notifyThirdJSDidLoad(resultCode => {
-        cb(resultCode)
-    });
+    static addReceiveThirdNotificationListener=(cb)=>{
+        listeners[cb] = DeviceEventEmitter.addListener('addReceiveThirdNotificationListener', cb);
+    }
+
+    static getLaunchAppNotification=(cb)=>{
+        return RNReactNativeMthirdpush.getLaunchAppNotification(res => {
+            cb(res)
+        });
+    }
+
+    static removeListener=(callback)=>{
+        if (!listeners[callback]) {
+            return
+        }
+        listeners[callback].remove()
+        listeners[callback] = null
+    }
 }
 
-export function addReceiveThirdNotificationListener(cb) {
-    DeviceEventEmitter.addListener('addReceiveThirdNotificationListener', cb);
-}
-
-export function getLaunchAppNotification(cb) {
-    return RNReactNativeMthirdpush.getLaunchAppNotification(res => {
-        cb(res)
-    });
-}
+export default Mthirdpush;
